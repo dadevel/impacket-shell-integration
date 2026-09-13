c210edd25afe329b5e4813d79d53ce0e1b07d9de
Daniel <dadevel@disroot.org>
2026-09-13 13:33:44 +0200

    update proxyconf.sh

--- proxyconf.sh
+++ proxyconf.sh
@@ -1,11 +1,11 @@
 proxyconf() {
     case "$#:$1" in
         4:set)
+            proxyconf unset
             declare -r protocol="$2"
             declare -r host="$3"
             declare -r port="$4"
-            declare -r config="${PROXYCHAINS_CONF_FILE:-/tmp/proxychains-$$.conf}"
-            cat << EOF > "${config}"
+            cat << EOF > "/tmp/proxychains-$$.conf"
 quiet_mode
 proxy_dns
 remote_dns_subnet 224
@@ -14,7 +14,7 @@ tcp_connect_time_out 1000
 [ProxyList]
 ${protocol} ${host} ${port}
 EOF
-            export LD_PRELOAD=/usr/lib/libproxychains4.so PROXYCHAINS_CONF_FILE="${config}" PROXYCHAINS_QUIET_MODE=1 PROXYCHAINS_ENDPOINT="${protocol}://${host}:${port}"
+            export LD_PRELOAD=/usr/lib/libproxychains4.so PROXYCHAINS_CONF_FILE="/tmp/proxychains-$$.conf" PROXYCHAINS_QUIET_MODE=1 PROXYCHAINS_ENDPOINT="${protocol}://${host}:${port}"
             ;;
         1:unset)
             if [[ "$PROXYCHAINS_CONF_FILE" == /tmp/proxychains-*.conf ]]; then
@@ -43,8 +43,7 @@ EOF
                 return 1
             fi
             (
-                unset LD_PRELOAD PROXYCHAINS_CONF_FILE PROXYCHAINS_QUIET_MODE PROXYCHAINS_ENDPOINT
-                PROXYCHAINS_CONF_FILE="$(mktemp /tmp/proxychains-XXXXXXXX.conf)" || exit 1
+                proxyconf unset
                 trap 'proxyconf unset' EXIT
                 proxyconf set "$2" "$3" "$4"
                 "${@:5}"
@@ -57,7 +56,7 @@ EOF
             echo '  set http|socks4|socks5 HOST PORT' >&2
             echo '  unset' >&2
             echo '  whereami' >&2
-            echo '  exec http|socks4|socks5 HOST PORT CMDLINE...' >&2
+            echo '  exec http|socks4|socks5 HOST PORT COMMAND...' >&2
             return 1
             ;;
     esac
